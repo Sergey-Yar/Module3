212f4da572bacc51b0cad616d3bf5ba90811101c
Сергей <ssergant@mail.ru>
2024-09-15 14:34:28 +0300

    добавил условие проверки 0 в конце числа

--- module_3_5.py
+++ module_3_5.py
@@ -2,11 +2,18 @@ def get_multiplied_digits(number):
     str_number = str(number)
     first = int(str_number[0])
     if len(str_number) > 1:
-        return first * get_multiplied_digits(int(str_number[1:]))
+        if int(str_number[:-1]) != 0:
+            return first * get_multiplied_digits(int(str_number[1:]))
+        else:
+
     else:
         return first
 
-result = get_multiplied_digits(40203)
-print(result)
-result = get_multiplied_digits(3)
-print(result)
+result1 = get_multiplied_digits(40203)
+print(result1)
+result2 = get_multiplied_digits(3)
+print(result2)
+result3 = get_multiplied_digits(330)
+print(result3)
+result4 = get_multiplied_digits(0)
+print(result4)
