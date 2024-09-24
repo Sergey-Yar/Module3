40aef1e3063b9b888dc2962e8c4bdbeb9113d12c
Сергей <ssergant@mail.ru>
2024-09-24 21:03:34 +0300

    переписал код, используя рекурсию

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,3 +1,28 @@
+
+def calculate_structure_sum(*args):
+    summ = 0
+    if not args:
+        return 0
+    else:
+        for i in args:
+            if isinstance(i, int):
+                summ += i
+                return summ
+            elif isinstance(i, str):
+                summ += len(i)
+                return summ
+            elif isinstance(i, list):
+                calculate_structure_sum(i)
+            elif isinstance(i, dict):
+                for key in i.keys():
+                    summ += len(key)
+                    return summ
+                for value in i.values():
+                    summ += value
+                    return summ
+            elif isinstance(i, tuple):
+                calculate_structure_sum(i)
+
 data_structure = [
   [1, 2, 3],
   {'a': 4, 'b': 5},
@@ -6,39 +31,4 @@ data_structure = [
   ((), [{(2, 'Urban', ('Urban2', 35))}])
 ]
 
-calculate_structure_sum = 0
-new_list = []
-
-for i in data_structure:
-    if isinstance(i, list):
-        calculate_structure_sum += sum(i)
-    elif isinstance(i, dict):
-        for key1 in i.keys():
-            calculate_structure_sum += len(key1)
-        calculate_structure_sum += sum(i.values())
-    elif isinstance(i, tuple):
-        for j in i:
-            if isinstance(j, int):
-                calculate_structure_sum += j
-            elif isinstance(j, dict):
-                for key2 in j.keys():
-                    calculate_structure_sum += len(key2)
-                calculate_structure_sum += sum(j.values())
-            elif isinstance(j, list):
-                for k in j:
-                    for k2 in k:
-                        for k3 in k2:
-                            if isinstance(k3, tuple):
-                                for k4 in k3:
-                                    if isinstance(k4, int):
-                                        calculate_structure_sum += k4
-                                    elif isinstance(k4, str):
-                                        calculate_structure_sum += len(k4)
-                            elif isinstance(k3, int):
-                                calculate_structure_sum += k3
-                            elif isinstance(k3, str):
-                                calculate_structure_sum += len(k3)
-    elif isinstance(i, str):
-        calculate_structure_sum += len(i)
-
-print('Сумма', calculate_structure_sum)
+print('Сумма', calculate_structure_sum(data_structure))
